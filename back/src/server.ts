79c58bcfbff35e609a7be2856709056c1c089c2e
Nicolas Mollerke <nicolasmollerke@gmail.com>
2026-09-18 20:16:00 -0300

    feat: adiciona o caminho para a rota Login

--- back/src/server.ts
+++ back/src/server.ts
@@ -5,6 +5,7 @@ import swaggerDocument from "../swagger-output.json";
 
 import routesFilmes from './routes/filmes'
 import routesClientes from './routes/clientes'
+import routesLogin from './routes/login'
 
 const app = express()
 const port = 3000
@@ -14,6 +15,7 @@ app.use(cors())
 
 app.use("/filmes", routesFilmes)
 app.use("/clientes", routesClientes)
+app.use("/clientes/login", routesLogin)
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
