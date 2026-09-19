4f522d5263c179319e6b496603265bd3796c96d4
Nicolas Mollerke <nicolasmollerke@gmail.com>
2026-09-19 14:32:45 -0300

    feat: adiciona o caminho para a rota Carrinho

--- back/src/server.ts
+++ back/src/server.ts
@@ -6,6 +6,7 @@ import swaggerDocument from "../swagger-output.json";
 import routesFilmes from './routes/filmes'
 import routesClientes from './routes/clientes'
 import routesLogin from './routes/login'
+import routesCarrinho from './routes/carrinhos'
 
 const app = express()
 const port = 3000
@@ -16,6 +17,7 @@ app.use(cors())
 app.use("/filmes", routesFilmes)
 app.use("/clientes", routesClientes)
 app.use("/clientes/login", routesLogin)
+app.use("/carrinho", routesCarrinho)
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
